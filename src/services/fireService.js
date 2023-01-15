d441ce0e599a7a2cffc4145a213c3785d5a22cda
Austin Ly <austinly@icloud.com>
2023-01-15 21:26:17 +1100

    Fixed missing input params and updated API

--- src/services/fireService.js
+++ src/services/fireService.js
@@ -1,29 +1,30 @@
 import { fetchFromApi } from "./utils";
 
 export const inputLabels = [
-    "Age",
-    "Income",
-    "Expenses",
-    "Super Contribution Rate",
-    "Deductions",
-    "Fringe Benefits",
-    "HELP Balance",
-    "Super Balance",
-    "Investments Balance",
-    "Assumed Growth",
+  "Age",
+  "Income",
+  "Expenses",
+  "Retirement Expenses",
+  "Super Contribution Rate",
+  "Deductions",
+  "Fringe Benefits",
+  "HELP Balance",
+  "Super Balance",
+  "Investments Balance",
+  "Assumed Growth",
 ];
 
 export const checkBoxLabels = [
-    "Super Inclusive",
-    "Private Hospital Cover",
-    "Max Super Contributions",
+  "Super Inclusive",
+  "Private Hospital Cover",
+  "Max Super Contributions",
 ];
 
 export const resultLabels = [
-    "Years To FIRE",
-    // "Age At FIRE"
+  "Years To FIRE",
+  // "Age At FIRE"
 ];
 
 export const fireProcessor = async (formData) => {
-    return await fetchFromApi("fire", formData);
+  return await fetchFromApi("fire", formData);
 };
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,49 +1,49 @@
+const API_ENDPOINT = "https://ausfin-api-production.up.railway.app/";
+// const API_ENDPOINT = "http://localhost:8080/";
+
 export const camelCase = (string) => {
-    return string
-        .split(" ")
-        .map((word, index) => {
-            if (index === 0) return word.toLowerCase();
-            return word[0].toUpperCase() + word.slice(1).toLowerCase();
-        })
-        .join("");
+  return string
+    .split(" ")
+    .map((word, index) => {
+      if (index === 0) return word.toLowerCase();
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join("");
 };
 
 export const fetchFromApi = async (endpoint, formData) => {
-    // Convert formData to an Object
-    const plainFormData = Object.fromEntries(formData.entries());
+  // Convert formData to an Object
+  const plainFormData = Object.fromEntries(formData.entries());
 
-    console.log(plainFormData);
-    // Convert object to JSON to send in body
-    const json = JSON.stringify(plainFormData).replaceAll('"on"', "true");
+  console.log(plainFormData);
+  // Convert object to JSON to send in body
+  const json = JSON.stringify(plainFormData).replaceAll('"on"', "true");
 
-    console.log(json);
+  console.log(json);
 
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: json,
-    };
-    const response = await fetch(
-        `https://ausfin-api.herokuapp.com/${endpoint}`,
-        options,
-    );
-    const result = await response.json();
-    return result;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: json,
+  };
+  const response = await fetch(`${API_ENDPOINT}${endpoint}`, options);
+  const result = await response.json();
+  return result;
 };
 
 export const flattenResult = (nested) => {
-    const result = {};
+  const result = {};
 
-    for (const [key, value] of Object.entries(nested)) {
-        if (typeof value != "object") {
-            result[key] = value;
-        } else {
-            for (const [subkey, subval] of Object.entries(value)) {
-                result[subkey] = subval;
-            }
-        }
+  for (const [key, value] of Object.entries(nested)) {
+    if (typeof value != "object") {
+      result[key] = value;
+    } else {
+      for (const [subkey, subval] of Object.entries(value)) {
+        result[subkey] = subval;
+      }
     }
-    return result;
+  }
+  return result;
 };
